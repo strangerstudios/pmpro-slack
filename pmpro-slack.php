a2310effcb31ca232170d88d3025afd270594b67
NikV <techvoltz@gmail.com>
2015-06-28 11:02:46 -0500

    Changes to the way messages are sent (using better user/level info)

--- pmpro-slack.php
+++ pmpro-slack.php
@@ -9,35 +9,38 @@
  * License: GNU GPLv2+
  */
 
-add_action( 'pmpro_after_change_membership_level', 'pmpro_change_membership_slack_integration');
+add_action( 'pmpro_after_change_membership_level', 'pmpro_change_membership_slack_integration', 10, 2);
 
 /**
  * Main Slack Integration Function
  *
+ * @param $level_id
+ * @param $user_id
+ *
  * @since 1.0
  */
-function pmpro_change_membership_slack_integration() {
+function pmpro_change_membership_slack_integration( $level_id, $user_id ) {
 
-
-	global $current_user;
-	$level = $current_user->membership_level;
-	$levelCost = $level->initial_payment;
+	$level = pmpro_getMembershipLevelForUser($user_id);
+	$current_user = get_userdata( $user_id );
+	$levelstuff = $current_user->membership_level;
+	$levelCost = $levelstuff->initial_payment;
 
 	$options = get_option('pmpro-slack-hook');
 	$webhook_url = $options['pmpro_slack_hook'];
 
 
 	// Check that webhook exists in the settings page
-	if ( !$webhook_url == "" ) {
+	if ( $webhook_url !== "" ) {
 		if ( is_user_logged_in() ) {
 			$payload = array(
-				'text'        => 'A user has changed their membership levels.' . $current_user->user_email,
+				'text'        => 'A user has changed their membership level. ' . $current_user->user_email,
 				'username'    => 'PMProBot',
 				'icon_emoji'  => ':credit_card:',
 				'attachments' => array(
 					'fields' => array(
 						'color' => '#8FB052',
-						'title' =>  $current_user->display_name . ' has signed up for membership level: ' . $current_user->membership_level->name . ' ($' . $levelCost . ')',
+						'title' =>  $current_user->display_name . ' has signed up for membership level: ' . $level->name . ' ($' . $levelCost . ')',
 					)
 				),
 			);
@@ -96,6 +99,7 @@ function pmpro_slack_integration_options_page() {
 }
 add_action('admin_init', 'pmpro_slack_admin_init');
 function pmpro_slack_admin_init(){
+
 	register_setting( 'pmpro-slack-group', 'pmpro-slack-hook', 'pmpro_slack_validate' );
 	add_settings_section( 'pmpro-slack-section', 'Slack Settings', 'pmpro_slack_section_callback', 'pmpro-slack' );
 	add_settings_field( 'pmpro_slack_field', 'Webhook URL', 'pmpro_slack_field_callback', 'pmpro-slack', 'pmpro-slack-section' );
@@ -108,6 +112,12 @@ function pmpro_slack_section_callback() {
 		<li>Copy the URL for the webhook</li>
 		<li>Paste the URL into the field below and click submit</li>
 		</ol>';
+	$roles = pmpro_getAllLevels(true, true);
+	foreach ($roles as $stud) {
+
+		echo $stud;
+	}
+
 }
 function pmpro_slack_field_callback() {
 	$setting = get_option( 'pmpro-slack-hook' );
